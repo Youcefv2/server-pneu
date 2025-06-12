16b884d157da6ea6ae91f5229be54bd4f657f611
Romain Benyoucef <87061742+Youcefv2@users.noreply.github.com>
2025-06-12 17:37:47 +0200

    */

--- app.js
+++ app.js
@@ -78,6 +78,7 @@ async function getEprelData(eprelCode) {
   let browser;
   try {
     browser = await puppeteer.launch({
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     args: [
     '--no-sandbox',
     '--disable-setuid-sandbox',

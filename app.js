99618b90d0c625cd71404a45de02cdbeec9d0fc8
Romain Benyoucef <87061742+Youcefv2@users.noreply.github.com>
2025-06-17 17:09:49 +0200

    process.env.PUPPETEER_EXECUTABLE_PATH

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ async function getEprelData(eprelCode) {
     '--single-process',
     '--no-zygote'
     ],
-    executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     headless: true
     });
 

5644a3b9ea7866eac379c5bc11dd2cc4dcd64a34
Romain Benyoucef <87061742+Youcefv2@users.noreply.github.com>
2025-06-17 15:29:57 +0200

    path env docker

--- app.js
+++ app.js
@@ -78,11 +78,13 @@ async function getEprelData(eprelCode) {
   let browser;
   try {
     browser = await puppeteer.launch({
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     args: [
     '--no-sandbox',
     '--disable-setuid-sandbox',
+    '--single-process',
+    '--no-zygote'
     ],
+    executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
     headless: true
     });
 
